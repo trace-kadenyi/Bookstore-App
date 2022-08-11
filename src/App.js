d98765a8c3b7bf21fa8fa9d3f970038aec89bebb
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-08-11 17:46:52 +0300

    Create components and navigation for the bookstore app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import React from 'react';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookList from './Components/Books/BookList';
+import Categories from './Components/Categories/Categories';
+import Navbar from './Components/Navigation/Navigation';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<BookList title="Purple Hibiscus" author="Chimamanda Ngozi" />} />
+      <Route exact path="/categories" element={<Categories />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
---
+++ src/Components/Categories/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Categories.css';
+
+const Categories = () => (
+  <div className="categories-container">
+    <button type="button" className="status">Check Status</button>
+  </div>
+);
+
+export default Categories;
