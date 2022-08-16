277f99555fabe2bc32c3d874b49a39af190a5ed2
TracK92 <40690789+TracK92@users.noreply.github.com>
2022-08-16 22:05:18 +0300

    Implement the Add Book button

--- src/Components/Books/Form.js
+++ src/Components/Books/Form.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/books';
 
-const Form = () => (
-  <form>
-    <input type="text" name="title" id="title" placeholder="BOOK TITLE" required />
-    <input type="text" name="author" id="author" placeholder="AUTHOR" required />
-    <button type="submit" className="submit">ADD BOOK</button>
-  </form>
-);
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const book = {
+      id: uuidv4(),
+      title,
+      author,
+      progress: 7,
+      chapter: 5,
+      genre: 'African Literature',
+    };
+    if (title.length && author.length) {
+      dispatch(addBook(book));
+      setTitle('');
+      setAuthor('');
+    }
+  };
+  return (
+    <form>
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+        name="title"
+        id="title"
+        value={title}
+        placeholder="BOOK TITLE"
+        required
+      />
+      <input
+        onChange={(e) => setAuthor(e.target.value)}
+        type="text"
+        name="author"
+        id="author"
+        value={author}
+        placeholder="AUTHOR"
+        required
+      />
+      <button
+        onClick={handleAdd}
+        type="submit"
+        className="submit"
+      >
+        ADD BOOK
+      </button>
+    </form>
+  );
+};
 
 export default Form;
